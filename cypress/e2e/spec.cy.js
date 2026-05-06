f79ecf916fa4de2c2e628ce7f7f58bef899d2038
gionacvlishvili2004-arch <gio.nacvlishvili2004@gmail.com>
2026-05-06 16:45:48 +0400

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,60 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Automation Exercise - Test Cases 1, 2, 3', () => {
+  let userData
+
+  before(() => {
+    cy.fixture('user').then((data) => {
+      userData = data
+      userData.validUser.email = `gio${Date.now()}@gmail.com`
+    })
+  })
+
+  beforeEach(() => {
+    cy.visit('https://automationexercise.com')
+  })
+
+  it('Test Case 1: Register User', () => {
+    cy.get('body').should('be.visible')
+
+    cy.openSignupLoginPage()
+
+    cy.contains('New User Signup!').should('be.visible')
+
+    cy.signupUser(userData.validUser.name, userData.validUser.email)
+
+    cy.contains('Enter Account Information').should('be.visible')
+
+    cy.fillRegisterForm(userData.validUser)
+
+    cy.url({ timeout: 10000 }).should('include', 'account_created')
+
+    cy.get('[data-qa="account-created"]')
+      .should('be.visible')
+      .and('contain', 'Account Created!')
+
+    cy.get('[data-qa="continue-button"]').click()
+
+    cy.contains(`Logged in as ${userData.validUser.name}`).should('be.visible')
+  })
+
+  it('Test Case 2: Login User with correct email and password', () => {
+    cy.openSignupLoginPage()
+
+    cy.contains('Login to your account').should('be.visible')
+
+    cy.loginUser(userData.validUser.email, userData.validUser.password)
+
+    cy.contains(`Logged in as ${userData.validUser.name}`, { timeout: 10000 })
+      .should('be.visible')
+  })
+
+  it('Test Case 3: Login User with incorrect email and password', () => {
+    cy.openSignupLoginPage()
+
+    cy.contains('Login to your account').should('be.visible')
+
+    cy.loginUser(userData.invalidUser.email, userData.invalidUser.password)
+
+    cy.contains('Your email or password is incorrect!', { timeout: 10000 })
+      .should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,35 +1,41 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('login', (loginName, password) => {
-  cy.visit('https://automationteststore.com/index.php?rt=account/login')
+Cypress.Commands.add('openSignupLoginPage', () => {
+  cy.contains('Signup / Login').click()
+})
 
-  cy.get('#loginFrm_loginname').clear().type(loginName)
-  cy.get('#loginFrm_password').clear().type(password)
+Cypress.Commands.add('signupUser', (name, email) => {
+  cy.get('[data-qa="signup-name"]').clear().type(name)
+  cy.get('[data-qa="signup-email"]').clear().type(email)
+  cy.get('[data-qa="signup-button"]').click()
+})
 
-  cy.get('button[title="Login"]').click()
+Cypress.Commands.add('loginUser', (email, password) => {
+  cy.get('[data-qa="login-email"]').clear().type(email)
+  cy.get('[data-qa="login-password"]').clear().type(password)
+  cy.get('[data-qa="login-button"]').click()
+})
 
-  cy.contains('My Account').should('be.visible')
+Cypress.Commands.add('fillRegisterForm', (user) => {
+  cy.get('#id_gender1').check()
+
+  cy.get('[data-qa="password"]').type(user.password)
+
+  cy.get('[data-qa="days"]').select(user.day)
+  cy.get('[data-qa="months"]').select(user.month)
+  cy.get('[data-qa="years"]').select(user.year)
+
+  cy.get('#newsletter').check()
+  cy.get('#optin').check()
+
+  cy.get('[data-qa="first_name"]').type(user.firstName)
+  cy.get('[data-qa="last_name"]').type(user.lastName)
+  cy.get('[data-qa="company"]').type(user.company)
+  cy.get('[data-qa="address"]').type(user.address)
+  cy.get('[data-qa="address2"]').type(user.address2)
+  cy.get('[data-qa="country"]').select(user.country)
+  cy.get('[data-qa="state"]').type(user.state)
+  cy.get('[data-qa="city"]').type(user.city)
+  cy.get('[data-qa="zipcode"]').type(user.zipcode)
+  cy.get('[data-qa="mobile_number"]').type(user.mobileNumber)
+
+  cy.get('[data-qa="create-account"]').click()
 })
